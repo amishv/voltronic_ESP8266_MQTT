60707080c6c62373df53e7fdac14fa886774af2d
Amish Vishwakarma <41266094+amishv@users.noreply.github.com>
2021-12-13 02:04:38 +0530

    Create passwd.h
    header containing all passwords and LAN info.

---
+++ include/passwd.h
@@ -0,0 +1,7 @@
+#define SSID      "SSID"
+#define WiFi_KEY  "passwd"
+#define SSID2      "SSID2"
+#define WiFi_KEY2  "passwd2"
+#define MQTT_HOST "you.rMQ.TTh.ost" //MQTT Host IP
+#define API_PASS  ""
+#define CLIENTID  "ESP-Solarmon"
